fb92289133f1f533f4853c943dbc27d12ba56b66
Shedrack Sunday <hannania002@gmail.com>
2023-01-09 20:53:36 +0100

    Add relationship item class

--- item.rb
+++ item.rb
@@ -3,15 +3,27 @@ require 'date'
 class Item
     attr_accessor :id, :archived, :label, :genre, :author, :source, :publish_date
 
-    def initialize id, archived, label, genre, author, source, publish_date
-        @id = id
+    def initialize( archived: false,  publish_date)
+        @id = Random.rand(1..100)
         @archived = archived
-        @label = label
-        @genre = genre
-        @author = author
-        @source = source
         @publish_date = publish_date
     end
+    
+   def label=(label)
+       @label = label
+       label.items.push(self) unless label.items.include?(self)
+   end
+
+   def gener=(gener)
+       @gener = gener
+       gener.items.push(self) unless gener.items.include?(self)
+   end
+   
+   def author=(author)
+       @author = author
+       author.items.push(self) unless author.items.include?(self)
+   end
+
 
     def can_be_archived?
         # Return true if published_date is older than 10 years, otherwise false
