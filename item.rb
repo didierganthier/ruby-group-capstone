847e3d74f2b7a09e53fdbdc87d9306bef86047b0
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-09 14:33:51 -0500

    Create Item class

--- item.rb
+++ item.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Item
     attr_accessor :id, :archived, :label, :genre, :author, :source, :publish_date
 
@@ -9,5 +11,15 @@ class Item
         @author = author
         @source = source
         @publish_date = publish_date
+    end
+
+    def can_be_archived?
+        # Return true if published_date is older than 10 years, otherwise false
+        (Date.today - publish_date).to_i / 365 > 10
+    end
+
+    def move_to_archive
+        # If the item can be archived, change the archived property to true
+        @archived = true if can_be_archived?
     end
 end
