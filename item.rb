b4f3bd3fe8e92c2e0b667ece6cc397e56960f92f
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-09 15:11:40 -0500

    Fix linters errors

--- item.rb
+++ item.rb
@@ -1,37 +1,37 @@
 require 'date'
 
 class Item
-    attr_accessor :id, :archived, :label, :genre, :author, :source, :publish_date
+  attr_accessor :publish_date
+  attr_reader :id, :archived, :label, :genre, :author, :source
 
-    def initialize( archived: false,  publish_date)
-        @id = Random.rand(1..100)
-        @archived = archived
-        @publish_date = publish_date
-    end
-    
-   def label=(label)
-       @label = label
-       label.items.push(self) unless label.items.include?(self)
-   end
+  def initialize(publish_date, archived: false)
+    @id = Random.rand(1..100)
+    @archived = archived
+    @publish_date = publish_date
+  end
 
-   def gener=(gener)
-       @gener = gener
-       gener.items.push(self) unless gener.items.include?(self)
-   end
-   
-   def author=(author)
-       @author = author
-       author.items.push(self) unless author.items.include?(self)
-   end
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
 
+  def gener=(gener)
+    @gener = gener
+    gener.items.push(self) unless gener.items.include?(self)
+  end
 
-    def can_be_archived?
-        # Return true if published_date is older than 10 years, otherwise false
-        (Date.today - publish_date).to_i / 365 > 10
-    end
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
 
-    def move_to_archive
-        # If the item can be archived, change the archived property to true
-        @archived = true if can_be_archived?
-    end
+  def can_be_archived?
+    # Return true if published_date is older than 10 years, otherwise false
+    (Date.today - publish_date).to_i / 365 > 10
+  end
+
+  def move_to_archive
+    # If the item can be archived, change the archived property to true
+    @archived = true if can_be_archived?
+  end
 end
