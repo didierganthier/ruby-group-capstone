c46af565c89d25cebef736b3f4fd3f1d5342d238
Shedrack Sunday <Hannania002@gmail.com>
2023-01-10 16:34:26 +0100

    Add Music Album class

---
+++ classes/Music_album.rb
@@ -0,0 +1,12 @@
+require_relative '../item'
+
+class MusicAlbum < Item
+  attr_accessor :name, :publish_date
+
+  def initialize(name, publish_date)
+    @id = Random.rand(1..100)
+    super(publish_date)
+    @name = name
+  end
+
+end
