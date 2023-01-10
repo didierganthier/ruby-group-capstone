d8c5d84c675681abb5fb11b0bc83b5f8feed4781
joker69216 <hamaraad883@gmail.com>
2023-01-10 09:56:42 +0300

    fix rubocop errors

--- game.rb
+++ game.rb
@@ -1,17 +1,11 @@
 require_relative './item'
 
 class Game < Item
-
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize (id, genre, author, source, publish_date, archived, multiplayer, last_played_at)
+  def initialize(publish_date, archived, multiplayer, last_played_at)
     super(publish_date, archived)
-    gener(genre)
-    author(author)
-    source(source)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
-
   end
-
 end
