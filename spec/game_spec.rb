d0b9c4d85ae3215087deb497fc344930516d89a6
joker69216 <hamaraad883@gmail.com>
2023-01-10 14:44:46 +0300

    fix the code to go along with item class fucntion

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,11 +3,20 @@ require 'date'
 
 describe '#Game' do
   it 'should initailise an object of game' do
-    date = Date.new
-    date = date.year - 5
-    put date
-    last_played = date.year + 2
-    put last_played
-    game = Game.new(date, false, true, )
+    date = Date.today
+    published  = date - 365 * 5
+    last_played = date - 365 * 3
+
+    game = Game.new(published, false, true, last_played)
+    expect(game).to be_an_instance_of(Game)
+  end
+
+  it 'should return true if the game can be archived' do
+    date = Date.today
+    published  = date - 365 * 5
+    last_played = date - 365 * 3
+
+    game = Game.new(published, false, true, last_played)
+    expect(game.can_be_archived?).to be true
   end
 end
