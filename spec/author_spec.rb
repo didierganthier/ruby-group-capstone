5660d1319085bb4f33b9378ce10d9353aabf9fd1
joker69216 <hamaraad883@gmail.com>
2023-01-10 12:04:32 +0300

    add basic tests

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -5,16 +5,4 @@ describe '#Author' do
     author = Author.new('John Doe')
     expect(author.name).to eq('John Doe')
   end
-
-  it 'should have a list of books' do
-    author = Author.new('John Doe')
-    expect(author.books).to eq([])
-  end
-
-  it 'should be able to add a book' do
-    author = Author.new('John Doe')
-    author.add_book('The Book')
-    expect(author.books).to eq(['The Book'])
-  end
 end
-
