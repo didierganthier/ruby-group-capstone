6a41150a132795e951130ef71fe8bb91b5012281
joker69216 <hamaraad883@gmail.com>
2023-01-10 14:46:06 +0300

    fix rubocop errors

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,8 +4,8 @@ require 'date'
 describe '#Game' do
   it 'should initailise an object of game' do
     date = Date.today
-    published  = date - 365 * 5
-    last_played = date - 365 * 3
+    published = date - (365 * 5)
+    last_played = date - (365 * 3)
 
     game = Game.new(published, false, true, last_played)
     expect(game).to be_an_instance_of(Game)
@@ -13,8 +13,8 @@ describe '#Game' do
 
   it 'should return true if the game can be archived' do
     date = Date.today
-    published  = date - 365 * 5
-    last_played = date - 365 * 3
+    published = date - (365 * 5)
+    last_played = date - (365 * 3)
 
     game = Game.new(published, false, true, last_played)
     expect(game.can_be_archived?).to be true
