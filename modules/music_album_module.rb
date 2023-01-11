1222aa403d8545a3baa0da2c8b6af174adbbbe5e
Shedrack-Sunday <hannania002@gmail.com>
2023-01-11 22:56:38 +0100

    Add changes to module files for export capability

--- modules/music_album_module.rb
+++ modules/music_album_module.rb
@@ -1,26 +1,24 @@
 require 'json'
 require_relative '../classes/music_album'
 
-module MusicAlbumModule
-  def load_music_albums
-    data = []
-    file = './json_files/music_album.json'
-    if File.exist?(file)
-      JSON.parse(File.read(file)).each do |music|
-        data.push(MusicAlbum.new(music['name'], music['publish_date'], music['on_spotify']))
-      end
-    else
-      File.write('./json_files/music_album.json', [])
+def load_music_albums
+  data = []
+  file = './json_files/music_album.json'
+  if File.exist?(file)
+    JSON.parse(File.read(file)).each do |music|
+      data.push(MusicAlbum.new(music['name'], music['publish_date'], music['on_spotify']))
     end
-
-    data
+  else
+    File.write('./json_files/music_album.json', [])
   end
 
-  def create_music_album
-    data = []
-    @music_albums.each do |album|
-      data.push({ name: album.name, publish_date: album.publish_date, on_spotify: album.on_spotify })
-    end
-    File.write('./json_files/music_album.json', JSON.generate(data))
+  data
+end
+
+def create_music_album
+  data = []
+  @music_albums.each do |album|
+    data.push({ name: album.name, publish_date: album.publish_date, on_spotify: album.on_spotify })
   end
+  File.write('./json_files/music_album.json', JSON.generate(data))
 end
